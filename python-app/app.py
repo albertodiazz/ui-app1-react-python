a2f385e7c1b88b8cfb5440bb46364941ea7c0569
alberto <testdesarrollo.a@gmail.com>
2022-02-25 02:03:32 -0600

    agregue flask para la funcion de api

--- python-app/app.py
+++ python-app/app.py
@@ -1,6 +1,30 @@
 from lib import pd
 from lib import config
 from lib import filterToRubro
+from lib import Flask, Api, CORS, Resource, reqparse
+
+
+app = Flask(__name__, template_folder='template/')
+api = Api(app)
+CORS(app)
+api_call = reqparse.RequestParser()
+
+
+# curl -X POST localhost:5000/fechas -G 'Content-Type: application/json' -d "msg=value"
+
+api_call.add_argument('mesA', type=str)
+api_call.add_argument('anoA', type=str)
+api_call.add_argument('mesB', type=str)
+api_call.add_argument('anoB', type=str)
+
+
+class rangosDeFechas(Resource):
+    def post(self):
+        args = api_call.parse_args()
+        print(args['mesA'], args['mesB'])
+
+
+api.add_resource(rangosDeFechas,'/fechas')
 
 
 if __name__ == '__main__':
@@ -8,4 +32,9 @@ if __name__ == '__main__':
     # Esta funcion solo se debe ejecutar una vez
     # y el momento para eso es cuando cambien los csv
     # del genesis
-    filterToRubro.run(config.CSV_BASE_INPC)
+    # -------------------------------- 
+    # -------------------------------- 
+    # filterToRubro.run(config.CSV_BASE_INPC)
+    # -------------------------------- 
+    # -------------------------------- 
+    app.run(debug=True)
--- python-app/lib/__init__.py
+++ python-app/lib/__init__.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
+from flask import Flask, jsonify
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS
+import requests
+
 from lib import config
 from lib import filterToRubro
