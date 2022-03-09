ae1320dfca5a14c37e5b74abe80a30afd68ae39a
alberto <testdesarrollo.a@gmail.com>
2022-03-08 21:24:05 -0600

    agregue una nota de descripcion en el boton de calculo

--- react-app/src/components/fechas/btnCalcular.jsx
+++ react-app/src/components/fechas/btnCalcular.jsx
@@ -3,7 +3,8 @@ import { Msg_Busqueda } from "../request/sendDatos"
 
 
 export default function BtnCalcular(props){
-
+	// Lo que hace este boton es mandar a llamar la data de todos lor rubors y 
+	// almacenarla en un objeto para su cosumo en lo que sigue de la aplicacion
 	// TODO: 
 	// [] Generar boton y enviar con sendDatos('msg': 'res') los datos
 	// [] Pasar variables de rango de busqueda
